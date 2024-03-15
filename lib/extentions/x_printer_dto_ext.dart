dac0753ab8145176965a875ee4ef243ea67284e3
KickNext <nikitiser@gmail.com>
2024-03-15 19:51:45 +0500

    Обновление версии на 0.0.4 в pubspec.yaml

---
+++ lib/extentions/x_printer_dto_ext.dart
@@ -0,0 +1,27 @@
+import 'package:pos_printers/pos_printers.pigeon.dart';
+
+extension XPrinterDTOExt on XPrinterDTO {
+  Map<String, dynamic> toJson() {
+    return {
+      'connectionType': connectionType.name,
+      'usbPath': usbPath,
+      'macAddress': macAddress,
+      'ipAddress': ipAddress,
+      'mask': mask,
+      'gateway': gateway,
+      'dhcp': dhcp,
+    };
+  }
+
+  XPrinterDTO fromJson(Map<String, dynamic> json) {
+    return XPrinterDTO(
+      connectionType: PosPrinterConnectionType.values.firstWhere((e) => e.name == json['connectionType']),
+      usbPath: json['usbPath'],
+      macAddress: json['macAddress'],
+      ipAddress: json['ipAddress'],
+      mask: json['mask'],
+      gateway: json['gateway'],
+      dhcp: json['dhcp'],
+    );
+  }
+}
